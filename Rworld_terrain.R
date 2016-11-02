3cb27533b15ca27bc65e6dc2c15f187e8b06ca20
Maggi <maggi@Maggis-MacBook-Pro.local>
2016-11-02 11:49:14 -0600

    Rworld

--- Rworld_terrain.R
+++ Rworld_terrain.R
@@ -13,3 +13,13 @@ matrix[1,5]<-ur
 matrix[5,1]<-ll
 matrix[5,5]<-lr
 
+### First diamond 
+#creating the center point
+md<- mean(ul, ur, ll, rr)
+md<- matrix[3,3]
+
+#creating the side points
+matrix[1,3]<- mean(ul,ur,md) 
+matrix[3,1]<- mean(ul, md, ll)
+matrix[5,3]<- mean(ur,md,lr)
+matrix[3,5]<- mean(ll,md,lr)
