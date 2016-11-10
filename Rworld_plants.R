09c1c38925dbe75bbb9f52f7dbb22f17e129c2a9
Maggi <maggi@Maggis-MacBook-Pro.local>
2016-11-10 09:45:41 -0700

    working

--- Rworld_plants.R
+++ Rworld_plants.R
@@ -13,17 +13,17 @@
 #comp.mat:matrix of spp. number of rows or columns reflects the number of species
 
 ##terrain matrix
-terr<- mat_func(3,3)
-terr[1,1]<-NA
-terr[1,2]<-2.2
-terr[1,3]<-3.4
-terr[2,1]<-.6
-terr[2,2]<-.5
-terr[2,3]<-.3
-terr[3,3]<-NA
-terr[3,2]<-4.4
-terr[3,1]<-1.8
-terr
+terrain<- mat_func(3,3)
+terrain[1,1]<-NA
+terrain[1,2]<-2.2
+terrain[1,3]<-3.4
+terrain[2,1]<-.6
+terrain[2,2]<-.5
+terrain[2,3]<-.3
+terrain[3,3]<-NA
+terrain[3,2]<-4.4
+terrain[3,1]<-1.8
+terrain
 
 ##making a comp.matrix
 mat_func<- function(x, y){
@@ -52,64 +52,86 @@ surv<-c(.2,.5,.6)
 #names
 nm<-c("a","b","c")
 ###checking life history parameters
-setup.plants <- function(repro, survive, comp.mat, names){
-  if(is.null(names))
-    names <- letters[seq_along(repro)]
+setup.plants <- function(repro, survive, comp.mat, name=NULL){
+  if(is.null(name))
+    name <- letters[seq_along(repro)]
   if(length(repro) != length(survive))
     stop("Reproduction and survival parameters needed for all species")
   if(length(repro) != ncol(comp.mat))
     stop("Reproduction and competition matrix needed for all species")
   if(ncol(comp.mat) != nrow(comp.mat))
     stop("number of col needs to match number of rows")
-  repro <- setNames(repro, names)
+  repro <- setNames(repro, name)
+  survive<- setNames(survive, name)
   return(list(repro=repro, survive=survive, comp.mat=comp.mat,
-              names=names))
+              name=name))
 }
-info<-setup.plants(repr, surv, c_mat, nm)
+info<-setup.plants(repr, surv, c_mat)
 info
-
+info$survive[]
+info$repro
 ###Suvival
-survive <- function(cell, info){
+survive <- function(cell, info, name){
   if(is.na(cell)) 
     return(NA)
   if (cell=='')
     return('')
-  if(runif(1) <= info$survive[plant])}
-    return(plants)
-    #$The plant survived! so do something...
+  if(runif(1) <= info$survive[name])
+    return(info$survive[name])
+  if(runif(1) >= info$survive[name])
+    return('')
 }
 
-survive(terr[2,1], info)
-ifelse(is.na(terr[2,1]), NA, info$survive)
-  
-is.na(terr[1,1])
+plants[2,2,1]<-survive(terrain[2,2], info, "b")
+plants
+
+#plant can be whatever you want
 
 
-    #$The plant survived! so do something...
+#how does it know survival is attached to that specific plant? Am I supposed to store the survival rate of the plant or the plant name?
+#this works if I define which survival rate to put into the function. So suvival needs to be attached to the plant?
 
 ###plant.timestep
 plant.timestep <- function(plants, terrain, info){
   survive <- function(cell, info){
-      if(is.na(cell)) 
-        return(NA)
-      if (cell=='')
-        return('')
-      if(runif(1) <= info$survive[plant])} #that plant survival probability
-       return(info$survive[plant])
+    if(is.na(cell)) 
+      return(NA)
+    if (cell=='')
+      return('')
+    if(runif(1) <= info$survive[plants])
+      return(plants)
     #$The plant survived! so do something...
-  for (i in ncol(survive.matrix)){
-    new.plants.matrix<- survive(cell, info)
+  }
+    #$The plant survived! so do something...
+  for (i in seq_len(dim(plants)[3])){
+    for (j in 1:ncol(plants)){
+    new.plants.matrix<- survive(j, info)
+    }
   }
   return(new.plants.matrix)
 }
 
+plant.timestep(plants, terrain, info)
+
+#how am I supposed to define the criteria in the second function? is new.plants.matrix the array? 
+
 ###run.plant.ecosystem
 #timesteps:assign a number
-timesteps<-
+timesteps<-3
 
 plants <- array("", dim=c(dim(terrain),timesteps+1))
 #...why timesteps+1, do you think?...
 for(i in seq_len(dim(plants)[3]))
   plants[,,i][is.na(terrain)] <- NA
 
+#so the array of plants is storing the name of the plants?
 
+###Reproduction
+plants <- reproduce(row, column, plants, info)
+
+reproduce <- function(row, col, plants, info){
+  possible.locations <- as.matrix(expand.grid(row+c(-1,0,1), col+c(-1,0,1)))
+  #...now filter out which ones are not water-logged and reproduce there...
+  #...being careful to check you do have somewhere to reproduce to!...
+  return(plants)
+}
